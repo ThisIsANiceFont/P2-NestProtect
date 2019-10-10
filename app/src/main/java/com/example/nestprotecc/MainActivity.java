196e1ae49d3169fda37a204eabea5de62b990835
Gene Pan <Pan90541>
2019-10-10 09:51:22 -0700

    added fragment to activity

--- app/src/main/java/com/example/nestprotecc/MainActivity.java
+++ app/src/main/java/com/example/nestprotecc/MainActivity.java
@@ -18,4 +18,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent (this, activity_lights.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/nestprotecc/fragment_menu.java
+++ app/src/main/java/com/example/nestprotecc/fragment_menu.java
@@ -15,7 +15,7 @@ public class fragment_menu extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflator.inflate(R.layout.menu_fragment, container, false);
+        View view = inflater.inflate(R.layout.menu_fragment, container, false);
         return view;
     }
 
