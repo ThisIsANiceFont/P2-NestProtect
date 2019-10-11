8d2a61a4e3f730be2b22b47946386d1ca300c8ae
Gene Pan <Pan90541>
2019-10-11 09:52:11 -0700

    all

--- app/src/main/java/com/example/nestprotecc/fragment_menu.java
+++ app/src/main/java/com/example/nestprotecc/fragment_menu.java
@@ -5,31 +5,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class fragment_menu extends Fragment {
+public class fragment_menu extends Fragment implements View.OnClickListener {
+    private Button mButton;
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu_fragment, container, false);
+
+        Button one = (Button) view.findViewById(R.id.button2);
+        one.setOnClickListener(this); // calling onClick() method
+        Button two = (Button) view.findViewById(R.id.button3);
+        two.setOnClickListener(this);
+        Button three = (Button) view.findViewById(R.id.button4);
+        three.setOnClickListener(this);
         return view;
     }
 
-    public void goToLights (View view){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                goToLights();
+                break;
+            case R.id.button3:
+                goToTemp();
+                break;
+            case R.id.button4:
+                goToSettings();
+                break;
+        }
+    }
+
+
+    public void goToLights (){
         Intent intent = new Intent (getActivity(), activity_lights.class);
         startActivity(intent);
     }
 
-    public void goToTemp (View view){
+    public void goToTemp (){
         Intent intent = new Intent (getActivity(), activity_temperature.class);
         startActivity(intent);
     }
 
-    public void goToSettings (View view){
+    public void goToSettings (){
         Intent intent = new Intent (getActivity(), activity_settings.class);
         startActivity(intent);
     }
